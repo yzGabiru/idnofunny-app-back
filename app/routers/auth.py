897fa6b6f3abde6d2ccdbfd8703fb1041017cc8e
Gabiru <paulohenriqueteodoro012@gmail.com>
2026-01-06 17:36:47 -0300

    ajuste email

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -27,12 +27,13 @@ conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_FROM=os.getenv("MAIL_FROM", "admin@idnofunny.com"),
-    MAIL_PORT=int(os.getenv("MAIL_PORT", 465)),
+    MAIL_PORT=int(os.getenv("MAIL_PORT", 587)),
     MAIL_SERVER=os.getenv("MAIL_SERVER", "smtp.gmail.com"),
-    MAIL_STARTTLS=os.getenv("MAIL_STARTTLS", "False").lower() == "true",
-    MAIL_SSL_TLS=os.getenv("MAIL_SSL_TLS", "True").lower() == "true",
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
     USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True
+    VALIDATE_CERTS=False, # Resolve o problema de travamento no Docker
+    TIMEOUT=30            # Dá mais tempo para conectar
 )
 
 # URL do Frontend (Ajuste se seu app rodar em outra porta, ex: 5173 ou 8100)
